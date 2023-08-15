b14a48ff16dcfaaf24be00941c05cc3557536c61
廖文忠 <wcliao1962@gmail.com>
2023-08-15 22:14:26 +0800

    修改Admin model & its migration

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Admin extends Model
 {
     use HasFactory;
+
+    protected $guard = 'admin';
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
