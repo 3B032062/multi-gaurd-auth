c47615aa34772260c73751217cca549061419ebe
廖文忠 <wcliao1962@gmail.com>
2023-08-21 12:22:04 +0800

    *再修Admin model改為extends Authenticatable(非常重要)

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
     use HasFactory;
 
@@ -18,4 +19,14 @@ class Admin extends Model
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+//    public function setPasswordAttribute($value)
+//    {
+//        $this->attributes['password'] = Hash::make('password');
+//    }
+
+    public function scopeIsActive($query)
+    {
+        return $query->where('is_active',1);
+    }
 }
